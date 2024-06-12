52d2a438ad853ed27776fa5b41d9b43adf9a61b9
Akinsanya Joel <akinsanyajoel82@.com>
2024-06-12 16:18:51 +0100

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user_route.js";
+import authRoutes from "./routes/auth_route.js";
 
 dotenv.config();
 
@@ -14,7 +15,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
--- api/routes/user_route.js
+++ api/routes/user_route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { test } from "../controllers/user_controllers.js";
+import { test } from "../controllers/user_controller.js";
 
 const router = express.Router();
 
