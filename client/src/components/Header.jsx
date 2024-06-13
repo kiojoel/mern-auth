3cec8bfb32befeb45593055ead832a90a94c436f
Akinsanya Joel <akinsanyajoel82@.com>
2024-06-13 01:04:31 +0100

    create signin ui and functionalities

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,10 +6,10 @@ export default function Header() {
     <div className="bg-gray-100">
       <div className="flex justify-between items-center max-w-6xl p-3 mx-auto">
         <Link to="/">
-          <h1 className="font-bold">App</h1>
+          <h1 className="font-extrabold">App</h1>
         </Link>
 
-        <ul className="flex gap-4">
+        <ul className="flex gap-4 font-bold">
           <Link to="/about">
             <li>About</li>
           </Link>
